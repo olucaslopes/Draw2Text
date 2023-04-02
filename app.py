5564cc592cb46351ee03201c8dc60cf02b21652b
Lucas Lopes <lucaslopesamorim@gmail.com>
2023-04-02 17:21:17 -0300

    Refactor app

--- app.py
+++ app.py
@@ -19,35 +19,22 @@ numbers and letters in drawing sketches
 # Specify canvas parameters in application
 drawing_mode = "freedraw"
 
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-
-realtime_update = st.sidebar.checkbox("Update in realtime", True)
+stroke_width = st.sidebar.slider("Stroke width: ", 1, 10, 3)
 
 # Create a canvas component
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
-    stroke_color=stroke_color,
-    background_color=bg_color,
-    update_streamlit=realtime_update,
+    stroke_color='#000000',
+    background_color='#EEEEEE',
+    update_streamlit=True,
     height=150,
     drawing_mode=drawing_mode,
     key="canvas",
 )
 
-# Do something interesting with the image data and paths
-if canvas_result.image_data is not None:
-    st.image(canvas_result.image_data)
-if canvas_result.json_data is not None:
-    objects = pd.json_normalize(canvas_result.json_data["objects"])  # need to convert obj to str because PyArrow
-    for col in objects.select_dtypes(include=['object']).columns:
-        objects[col] = objects[col].astype("str")
-    st.dataframe(objects)
 
-
-if st.sidebar.button('Save Image'):
+if st.sidebar.button('Predict Digits'):
     save_png(canvas_result.image_data)
 
     # with open('current_image_array.npy', 'wb') as f:
@@ -55,7 +42,3 @@ if st.sidebar.button('Save Image'):
 
     fig = predict_drawings(canvas_result.image_data)
     st.pyplot(fig=fig)
-
-
-
-
